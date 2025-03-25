22c2d093af9708e6031a3f78fe9460e5a1c21bb6
Coder3451 <segniwoldemichael@gmail.com>
2025-03-25 23:30:48 +0300

    Update 0-add.py 1-calculation.py and 2-args.py to meet requirement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
     length = len(argv) - 1
 
     if length == 0:
-        print("{}: arguments.".format(length))
+        print("{} arguments.".format(length))
     elif length == 1:
-        print("{}: argument:".format(length))
+        print("{} argument:".format(length))
     else:
-        print("{}: arguments.".format(length))
+        print("{} arguments.".format(length))
 
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
