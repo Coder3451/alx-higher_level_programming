792d72843f98042abccb739173e7ebb183125a28
Coder3451 <segniwoldemichael@gmail.com>
2025-03-25 23:25:02 +0300

    Update to meet requirement

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,17 +2,15 @@
 
 from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
-
-result = sub(a, b)
-print("{} + {} = {}".format(a, b, result))
-
-result = mul(a, b)
-print("{} + {} = {}".format(a, b, result))
-
-result = div(a, b)
-print("{} + {} = {}".format(a, b, result))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+    result = sub(a, b)
+    print("{} + {} = {}".format(a, b, result))
+    result = mul(a, b)
+    print("{} + {} = {}".format(a, b, result))
+    result = div(a, b)
+    print("{} + {} = {}".format(a, b, result))
