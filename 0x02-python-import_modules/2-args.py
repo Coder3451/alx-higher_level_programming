d0d5294c6cb5d1595674c18afc5bd37aa1b8a87e
Coder3451 <segniwoldemichael@gmail.com>
2025-03-25 11:57:26 +0300

    Finishing the tasks of Python import modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+if __name__ == "__main__":
+    length = len(argv) - 1
+
+    if length == 0:
+        print("{}: arguments.".format(length))
+    elif length == 1:
+        print("{}: argument:".format(length))
+    else:
+        print("{}: arguments.".format(length))
+
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
