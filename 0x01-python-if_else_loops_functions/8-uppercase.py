eb08f443ed5aab470900752b9a7f053319126514
Coder3451 <segniwoldemichael@gmail.com>
2025-03-25 10:46:04 +0300

    Update tasks to meet requirements

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def uppercase(str):
     for ch in str:
         if ord('a') <= ord(ch) <= ord('z'):
             ch = chr(ord(ch) - 32)
-        print(f"{chr}", end="")
+        print("{}".format(ch), end="")
     print()
