81ce6ae0ef96fffe9c5a436f1f5a84a2984bba87
Coder3451 <segniwoldemichael@gmail.com>
2025-04-05 14:14:48 +0300

    Remove importing sys and using stderr to meet expected output

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import sys
 
 
 def safe_print_integer_err(value):
@@ -7,5 +6,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print("Expection: {}".format(e), file=sys.stderr)
+        print("Expection: {}".format(e))
         return False
