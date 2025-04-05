24a0f1b9f0b23487a36b9ca3c08373b691e4c349
Coder3451 <segniwoldemichael@gmail.com>
2025-04-05 13:46:59 +0300

    Correct typo of variable name 'i'

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
     result = [0] * list_length
     for i in range(list_length):
         try:
-            result[l] = my_list_1[i] / my_list_2[i]
+            result[i] = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
         except ZeroDivisionError:
