72e008dff39aa465f689a42833550f50088f9379
Coder3451 <segniwoldemichael@gmail.com>
2025-04-05 13:11:02 +0300

    Correct indentation on line 9

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,8 +6,8 @@ def safe_print_list_integers(my_list=[], x=0):
     i = 0
     while i < x:
         try:
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
         except (TypeError, ValueError):
             pass
         i += 1
